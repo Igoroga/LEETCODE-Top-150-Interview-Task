f5116f8e4417160d09ff6a0e5321bfc1f4f5da11
Igor Gusakov <igorgusakov199918@gmail.com>
2023-08-09 00:10:27 +0600

    9

--- task8.js
+++ task8.js
@@ -1,5 +1,25 @@
-let s = [1,2,3,4,5,6,7,8]
-// [1,2,2,2, , 6,7,8]
-s.splice(2, 3, 2, 2)
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
-console.log(s);
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+// However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+
+function maxProfit(prices) {
+    let maxProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1];
+        }
+    }
+    console.log(maxProfit);
+    return maxProfit;
+}
+
+
+maxProfit([7,1,5,3,6,10 ,4])
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
